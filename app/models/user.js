a0b93fc627a5850af49837efa3415b6c378f2d2e
unknown <leeterri1219@gmail.com>
2021-06-01 12:20:12 +0200

    lots of code changed. there was a big bug. must commit with better practice next time

--- app/models/user.js
+++ app/models/user.js
@@ -11,11 +11,53 @@ export default class UserModel extends Model {
     /* NOTE: confirm returns true if press ok and false when press cancel
 	TO DO: when the function returns true, change value to !value, otherwise, do nothing
      */
-    confirm = confirm("Archive this user?")
+
+
+
+     
+     // Code to examine below ID: 001
+     /*
+    var confirm = confirm("Archive this user?");
     if (confirm){
     	this.archived = !this.archived;
     }
+    else{
+      this.archived = this.archived; 
+    }
+    */
 
-    /* after archived
+    /* 001 BUG: when one user is archived the other users can not be archived*/
+    // TEST1: try confirm function without if statement
+    //confirm("testing")
+    // RESULT: confirm function without if statement is working fine
+
+    // TEST2: assign confirm reult to variable
+    // var confirmResult = confirm("testing");
+    // RESULT: working fine
+
+    // TEST3: add if statement below the confirm code
+    /*
+    var confirmResult = confirm("testing");
+    if (confirmResult){
+      this.archived = !this.archived;
+    }
+    */
+    // RESULT: working fine now..
+
+
+    // TO DO: If this.archived is true, confirm prompt a different: Do you want to un-archive?
+    if (!this.archived){
+      var confirmResult = confirm("Archive user?");
+      if (confirmResult){
+        this.archived = !this.archived;
+      }
+    } else {
+      var confirmResult = confirm("Un-archive user?");
+      if (confirmResult){
+        this.archived = !this.archived;
+      }
+    }
+    
+
   }
 }
